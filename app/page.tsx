0f9e4dcc2b85a77a632ce78791bbdfeb8caca9ff
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-03 11:41:35 +0700

    add hover div

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import Image from 'next/image'
 import { Bungee_Tint } from "next/font/google";
 
-const bungee = Bungee_Tint({ 
+const bungee = Bungee_Tint({
   subsets: ["latin"],
   weight: "400",
 });
 
 export default function MyApp() {
-  return (<div 
-    className="w-[80%] border rounded p-4 m-auto bg-slate-100 
-     grid 
-    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4"
-    >
-      {/* grid-cols-1 gap-4 
+  return (<div>
+    {/* grid-cols-1 gap-4 
       sm:grid-cols-2
       md:grid-cols-3
       lg:grid-cols-4
@@ -20,32 +16,37 @@ export default function MyApp() {
 
       flex flexwrap */}
 
-    <h1 className={`${bungee.className} "text-4xl font-bold m-2 p-2"`}>Hello world!!</h1>
+    <h1 className={`${bungee.className} "font-bold m-2 p-2"`}>Hello world!!</h1>
 
-    <Image
-      className='p-2 m-4'
-      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
-      width={150}
-      height={150}
-      alt="PSU Logo"
-    />
+    <div className="w-[80%] border rounded p-4 m-auto bg-slate-100 
+     grid 
+    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+      <Image
+        className='p-2 m-4'
+        src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+        width={150}
+        height={150}
+        alt="PSU Logo"
+      />
 
-    <Image
-      className="p-2 m-4"
-      src="/next.svg"
-      width={150}
-      height={150}
-      alt="Next.js Logo"
-    />
+      <Image
+        className="p-2 m-4"
+        src="/next.svg"
+        width={150}
+        height={150}
+        alt="Next.js Logo"
+      />
 
-    <div className="w-64 p-2 border mb-2 rounded bg-blue-200">
-      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-200 hover:bg-blue-700 hover:text-blue-50">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border rounded bg-amber-300">
+        Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
+      </div>
     </div>
-    <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
-      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
-    </div>
-    <div className="w-64 p-2 border rounded bg-amber-300">
-      Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
-    </div>
+
   </div>)
 }
