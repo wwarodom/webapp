12fea955a3c5738bd5775274ba5bcb607a103f92
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-03 11:08:16 +0700

    add image url

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+import Image from 'next/image'
+
 export default function MyApp() {
-  return (<div>
-    <h1>Hello world!!</h1>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
-    </p>
+  return (<div className="w-[80%] border rounded p-4 m-auto">
+    <h1 className="text-2xl font-bold m-2 p-2">Hello world!!</h1>
+
+    <Image
+      className='p-2 m-4'
+      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+      width={150}
+      height={150}
+      alt="PSU Logo"
+    />
+
+    <Image
+      className="p-2 m-4"
+      src="/next.svg"
+      width={150}
+      height={150}
+      alt="Next.js Logo"
+    />
+
+    <div className="w-64 p-2 border mb-2 rounded bg-blue-200">
+      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+    </div>
+    <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
+      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+    </div>
+    <div className="w-64 p-2 border rounded bg-amber-300">
+      Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
+    </div>
   </div>)
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'computing.psu.ac.th',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
