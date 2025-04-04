abed535b736ed6603fd392904496d4f093697208
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-04 09:56:16 +0700

    add more products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,7 +2,7 @@ export default function Products() {
     return (
         <div>
             <h1 className="font-bold text-xl m-4">Product</h1>
-            <div className="w-[80%] border rounded m-4 p-4 flex bg-slate-100">
+            <div className="flex flex-wrap m-auto justify-center w-[80%] border rounded m-4 p-4 bg-slate-100">
                 <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
                     <h2>1. Computer:</h2>
                     <p>Product description goes here.</p>
@@ -13,8 +13,18 @@ export default function Products() {
                     <p>Product description goes here.</p>
                     <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
                 </div>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>3. Head phone:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>4. iPad 4th Gen:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
             </div>
-            <footer>--- Product footer --- </footer>
+            <footer className="bg-blue-200 p-2 text-center font-bold text-xl"> ..:: Product footer ::.. </footer>
         </div>
     )
 }
