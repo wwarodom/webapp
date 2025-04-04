0ccfb3c4d6d58d960c4b42f8e75cd1adf8373889
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-04 09:48:31 +0700

    update product

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,17 @@
 export default function Products() {
     return (
         <div>
-            <h1>Product</h1>
+            <h1 className="font-bold text-xl m-4">Product</h1>
             <div className="w-[80%] border rounded m-4 p-4 flex bg-slate-100">
-                <div className="border w-fit bg-green-200 p-2 m-2">
-                    <h2>Computer:</h2>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>1. Computer:</h2>
                     <p>Product description goes here.</p>
-                    <button className="border bg-blue-200">Buy Now</button>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
                 </div>
-                <div className="border w-fit bg-green-200 p-2 m-2">
-                    <h2>Laptop core i7:</h2>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>2. Laptop core i7:</h2>
                     <p>Product description goes here.</p>
-                    <button className="border bg-blue-200">Buy Now</button>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
                 </div>
             </div>
             <footer>--- Product footer --- </footer>
