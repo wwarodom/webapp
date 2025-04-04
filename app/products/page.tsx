d2b02e54ea7ce5c0ddfd65cf74ccc33ef9290071
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-04 10:47:40 +0700

    add JSON Object

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,30 +1,29 @@
 export default function Products() {
-    return (
-        <div>
-            <h1 className="font-bold text-2xl text-center my-4">Product</h1>
-            <div className="flex flex-wrap m-auto justify-center w-[80%] border rounded m-4 p-4 bg-slate-100 mb-6">
-                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
-                    <h2>1. Computer:</h2>
-                    <p>Product description goes here.</p>
-                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
-                </div>
-                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
-                    <h2>2. Laptop core i7:</h2>
-                    <p>Product description goes here.</p>
-                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
-                </div>
-                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
-                    <h2>3. Head phone:</h2>
-                    <p>Product description goes here.</p>
-                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
-                </div>
-                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
-                    <h2>4. iPad 4th Gen:</h2>
-                    <p>Product description goes here.</p>
-                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
-                </div>
-            </div>
-            <footer className="bg-blue-200 p-2 text-center font-bold text-xl"> ..:: Product footer ::.. </footer>
+
+    const products = [
+        { name: "Computer", description: "Product description computer goes here." },
+        { name: "Laptop core i7", description: "Product description laptop goes here." },
+        { name: "Head phone", description: "Product description phone goes here." },
+        { name: "iPad 4th Gen", description: "Product description ipad goes here." },
+        { name: "iPhone 4th Gen", description: "Product description iphone goes here." },
+    ];
+    return (<div>
+        <h1 className="font-bold text-2xl text-center my-4">Product</h1>
+        <div className="flex flex-wrap m-auto justify-center w-[80%] border rounded m-4 p-4 bg-slate-100 mb-6">
+            {
+                products.map((product, index) => {
+                    return (
+                        <div key={index} className="shadow-lg rounded-md border w-fit bg-green-100 p-2 m-2">
+                            <h2>{index + 1}. {product.name}</h2>
+                            <p>{product.description}</p>
+                            <button className="border bg-pink-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                        </div>)
+                })
+            }
+
+
         </div>
+        <footer className="bg-blue-200 p-2 text-center font-bold text-xl"> ..:: Product footer ::.. </footer>
+    </div>
     )
 }
