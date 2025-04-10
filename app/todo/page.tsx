45660a27715b144ff78f9cde3d50ce837fe3e4e7
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-10 11:41:10 +0700

    add new task

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -10,6 +10,19 @@ export default function Todo() {
         { id: 3, title: 'Sleeping', complete: true },
     ])
 
+    const [task, setTask] = useState('')
+
+    const addTask = () => {
+        if (!task) return
+        const newTask = {
+            id: todos.length + 1,
+            title: task,
+            complete: false
+        }
+        setTodos([...todos, newTask])
+        setTask('')
+    }
+
     return (<div className="border-2 max-w-sm border-gray-300 rounded-lg mx-auto p-4">
         <h1 className="text-xl font-bold">Todo</h1>
         <div>
@@ -17,19 +30,25 @@ export default function Todo() {
                 {todos.map(todo => (
                     <li key={todo.id} className="my-2">
                         <span>{todo.id}. </span>
-                        <span>{todo.title}</span>
+                        <span className="mr-2"
+                        >{todo.title}</span>
                         <input
                             type="checkbox"
-                            checked={todo.complete} />
+                            defaultChecked={todo.complete} />
                     </li>
                 ))}
             </ul>
-            <input 
-                className="border-2 border-gray-300 rounded-lg p-2 m-2" 
-                type="text" placeholder="Add a new todo" />
+ 
+            <input
+                className="border-2 border-gray-300 rounded-lg p-2 m-2"
+                type="text"
+                value={task}
+                onChange={e => setTask(e.target.value)}
+                placeholder="Add a new todo" />
             <button
+                onClick={addTask}
                 className="border-2 border-gray-300 rounded-lg p-2 m-2 bg-blue-500 text-white"
-                >Add</button>
+            >Add</button>
         </div>
     </div>)
 }
