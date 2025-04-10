f40b4da2c888f8a9aeb8a3a714b1730fe0a1891f
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-10 10:51:19 +0700

    update UI

--- app/counter/Person.tsx
+++ app/counter/Person.tsx
@@ -2,38 +2,59 @@
 
 import { useState } from "react"
 
-export default function Person() {  
+export default function Person() {
 
-    const [name, setName] = useState("Warodom") 
+    const [name, setName] = useState("Warodom")
 
     function handleName(e: React.ChangeEvent<HTMLInputElement>) {
         console.log(e.target.value)
         setName(e.target.value)
     }
 
-    // Task: create other 2 input:
+    // Task: create other 2 inputs:
     // 1. age
     // 2. email
 
-    
+
     return (
-        <div>
-            <div className="max-w-sm border border-gray-200 rounded-lg shadow-md p-4 mx-auto">
-                <div>
-                Person: {name}
-                </div>
-                
-                <label 
+        <div className="max-w-sm border border-gray-200 rounded-lg shadow-md p-4 mx-auto">
+            <div className="bg-gray-200 p-2 rounded-md mb-2">
+                <b>Person:</b> {name}
+            </div>
+            <div> 
+                <label
                     htmlFor="name">Name</label>
-                <input 
-                    className="border p-2 w-full rounded-md"
-                    type="text" 
+                <input
+                    className="border p-2 w-full rounded-md mb-4"
+                    type="text"
                     name="name"
-                    value={name} 
+                    value={name}
                     onChange={handleName}
                 />
             </div>
-            
+            <div> 
+                <label
+                    htmlFor="name">Age</label>
+                <input
+                    className="border p-2 w-full rounded-md mb-4"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleName}
+                />
+            </div>
+            <div> 
+                <label
+                    htmlFor="name">Email</label>
+                <input
+                    className="border p-2 w-full rounded-md"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleName}
+                />
+            </div>
+
         </div>
     )
 }
