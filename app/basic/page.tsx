4afc2dd78b92544b7e2b5ce66e9ed5cf29ac3347
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-10 09:42:02 +0700

    curried function

--- app/basic/page.tsx
+++ app/basic/page.tsx
@@ -1,6 +1,4 @@
-export default function Basic() {
-     
-
+export default function Basic() { 
     const objA = {
         age: 23,
     }
@@ -16,14 +14,40 @@ export default function Basic() {
         surname: "Werapun",
         myObject: objA,
         myArray: [1, 2, 3],
-        myFoo: foo,
-        myBar: function () {
+        myFoo: foo,  // traditional function
+        myBar: function () { // anonymous function
             return "Bar"
         },
-        myBaz: () => "Baz"
+        myBaz: () => "Baz"  // arrow function
     }
     person.name = "Donald"
-    person.surname = "Trump" 
+     
+
+    function sum(a: number, b: number, c: number) {
+        return a+b+c
+    }
+
+    const result = sum(1, 2, 3)
+    console.log(result) // 6
+
+    // Curried function
+    const sum0 = (a: number) => {
+        return (b: number) => {
+            return (c: number) => {
+                return a + b + c
+            }
+        }
+    }
+    console.log(sum0(1)(2)(4)) // 6 
+    
+    const sum1 = (a:number) => 
+        (b:number) => 
+            (c:number) => 
+                a + b + c
+    console.log(sum1(1)(2)(3)) // 6
+
+
+
     return (<div> {person.name} {person.surname}
         <br />
         {person.myObject.age}
