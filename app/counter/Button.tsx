ec72d41e291a4fc7d4d6aa96ab8fdd26e0508f0d
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-10 10:34:27 +0700

    solution

--- app/counter/Button.tsx
+++ app/counter/Button.tsx
@@ -2,21 +2,38 @@
 
 import { useState } from "react"
 
-export function Button() {
+export default function Button() {
 
     const [count, setCount] = useState(0)
 
     function add() {
         setCount(count + 1)
     }
+    function minus() {
+        setCount(count - 1)
+    }
+    function reset() {
+        setCount(0)
+    }
 
     return (
         <div>
             <h2>Count: {count}</h2>
-            <button
-                onClick={add}
-                className="bg-blue-200 p-2 border"
-            > +1 </button>
+            <div >
+                <button
+                    onClick={add}
+                    className="bg-blue-200 p-2 border mr-2"
+                > +1 </button>
+                <button
+                    onClick={minus}
+                    className="bg-orange-200 p-2 border mr-3"
+                > -1 </button>
+                <button
+                    onClick={reset}
+                    className="bg-red-100 p-2 border"
+                > 0 </button>
+            </div>
+
         </div>
 
     )
