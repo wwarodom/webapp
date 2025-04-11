5f90595acd8204e6e4d40a6008aa9a82aee40b79
Warodom WERAPUN <wwarodom@gmail.com>
2025-04-11 11:03:20 +0700

    add init json value

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -4,8 +4,44 @@ import { useState } from 'react'
 
 export default function FetchAPI() {
 
+    const init = {
+        "login": "wwarodom",
+        "id": 6958879,
+        "node_id": "MDQ6VXNlcjY5NTg4Nzk=",
+        "avatar_url": "https://avatars.githubusercontent.com/u/6958879?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/wwarodom",
+        "html_url": "https://github.com/wwarodom",
+        "followers_url": "https://api.github.com/users/wwarodom/followers",
+        "following_url": "https://api.github.com/users/wwarodom/following{/other_user}",
+        "gists_url": "https://api.github.com/users/wwarodom/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/wwarodom/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/wwarodom/subscriptions",
+        "organizations_url": "https://api.github.com/users/wwarodom/orgs",
+        "repos_url": "https://api.github.com/users/wwarodom/repos",
+        "events_url": "https://api.github.com/users/wwarodom/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/wwarodom/received_events",
+        "type": "User",
+        "user_view_type": "public",
+        "site_admin": false,
+        "name": "Warodom Werapun",
+        "company": "CoE, PSU",
+        "blog": "https://werapun.com/",
+        "location": "Phuket, Thailand",
+        "email": null,
+        "hireable": true,
+        "bio": "CoE Lecturer",
+        "twitter_username": null,
+        "public_repos": 144,
+        "public_gists": 42,
+        "followers": 131,
+        "following": 45,
+        "created_at": "2014-03-15T10:42:07Z",
+        "updated_at": "2025-03-21T02:24:30Z"
+        }
+
     const [name, setName] = useState('wwarodom')
-    const [profile, setProfile] = useState({})
+    const [profile, setProfile] = useState(init)
 
 
     const fetchAPI = async () => {
